841f78ea0da1941fb6acc2af07556763f31278a4
navetacandra <naveta.cand@gmail.com>
2024-10-27 06:57:33 +0700

    fix: update vite config

--- ui-client/vite.config.js
+++ ui-client/vite.config.js
@@ -4,4 +4,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true,
+        secure: false,
+        ws: true
+      }
+    }
+  }
 })
