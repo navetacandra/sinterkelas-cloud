2bb0c47b64af144ce2094a93fffc146a67b3350e
navetacandra <naveta.cand@gmail.com>
2024-10-27 06:54:05 +0700

    feat: setup route for ui-client

--- ui-client/src/App.svelte
+++ ui-client/src/App.svelte
@@ -1,1 +1,13 @@
-<h1 class="text-3xl font-bold">Hello World from Tailwind!</h1>
+<script>
+  import { Router, Route, Link } from "svelte-routing";
+  import LoginPage from "./pages/Login.svelte";
+</script>
+
+<Router>
+  <Route path="/">
+    <h1 class="text-3xl font-bold">Hello World from Tailwind!</h1>
+  </Route>
+  <Route path="/login">
+    <LoginPage />
+  </Route>
+</Router>
