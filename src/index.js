92d3fb81ecb14c1dc516e852726d6d8617d75add
navetacandra <naveta.cand@gmail.com>
2024-10-27 02:12:14 +0700

    feat: send file with compression and handle cache file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const { spawnSync } = require('child_process');
 const path = require('path');
 const express = require('express');
-const { existsSync } = require('fs');
+const { existsSync, createReadStream } = require('fs');
+const compression = require('compression');
+const { createHash } = require('crypto');
 
 // build-ui.js
 ;(() => {
@@ -12,6 +14,31 @@ const { existsSync } = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(compression({ level: 9, filter: () => true }));
+
+// Function for handle file cache
+function handleFileCache(req, res, filepath) {
+  const hash = createHash('md5'); // Generate md5 hash
+  const fileStream = createReadStream(filepath); // Read file
+  fileStream.on('data', (chunk) => hash.update(chunk)); // Update hash
+  fileStream.on('end', () => {
+    const etag = hash.digest('hex'); // Get hash
+    res.setHeader('Cache-Control', 'public, max-age=86400'); // Set cache to 1 day
+    res.setHeader('ETag', etag); // Set hash as etag
+    if(req.headers['if-none-match'] === etag) {
+      res.status(304).send(); // Send 304 if etag match
+    } else {
+      res.sendFile(filepath); // Send file
+    }
+  });
+  fileStream.on('error', (err) => {
+    console.error(err);
+    res.status(500).send(); // Send 500 if error
+  });
+}
 
 app.get('/*', (req, res) => {
   // Get request path
@@ -31,7 +58,7 @@ app.get('/*', (req, res) => {
   }
 
   // Send the file
-  return res.sendFile(absPath);
+  handleFileCache(req, res, absPath)
 })
 
 app.listen(PORT, () => console.log(`Server listen on port: ${PORT}`))
