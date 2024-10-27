a7779b0010c9bdb2140a02c352ec0d8a01c3cd36
navetacandra <naveta.cand@gmail.com>
2024-10-27 12:29:19 +0700

    feat: create service worker generator

--- src/build-ui.js
+++ src/build-ui.js
@@ -1,5 +1,8 @@
 const { spawn } = require('child_process');
+const { readdirSync, statSync, readFileSync, writeFileSync, copyFileSync } = require('fs');
 const path = require('path');
+const uiClientPath = path.resolve(path.join(__dirname, '..', 'ui-client'));
+const distPath = path.join(uiClientPath, 'dist');
 
 function execute(command, args, options) {
   return new Promise((resolve, reject) => {
@@ -21,13 +24,52 @@ function execute(command, args, options) {
 }
 
 function buildUI() {
-  return execute('npm', ['run', 'build'], { cwd: path.resolve(path.join(__dirname, '..', 'ui-client')) });
+  return execute('npm', ['run', 'build'], { cwd: uiClientPath });
+}
+
+function getDistItems(dirpath = distPath) {
+  const items = readdirSync(dirpath);
+  let paths = [];
+  items.forEach(item => {
+    const itemPath = path.join(dirpath, item);
+    const stat = statSync(itemPath);
+    if(stat.isDirectory()) {
+      return getDistItems(itemPath).forEach(path => paths.push(path));
+    }
+    paths.push(itemPath);
+  });
+  return paths.map(p => p.replace(distPath, '')).sort();
+}
+
+function generateSW() {
+  const items = getDistItems();
+  const svelteApp = readFileSync(path.join(uiClientPath, 'src', 'App.svelte'), 'utf8');
+  const pathRegisterated = svelteApp.match(/path="(.*)"/g);
+  if(pathRegisterated) {
+    const paths = pathRegisterated.map(p => p.replace(/path="(.*)"/, '$1'));
+    items.push(...paths);
+  }
+
+  let oldCacheData = 'const cacheData = ["/sw.js"];';
+  let newCacheData = `const cacheData = [\n  "/sw.js",\n${items.sort().map(i => `  "${i}"`).join(',\n')}\n];`;
+  const swTemplate = readFileSync(path.join(__dirname, 'sw.template.js'), 'utf8');
+  const newSW = swTemplate.replace(oldCacheData, newCacheData);
+  writeFileSync(path.join(distPath, 'sw.js'), newSW);
+}
+
+function generateServiceWorker() {
+  copyFileSync(path.join(__dirname, 'service-worker.template.js'), path.join(distPath, 'service-worker.js'));
+  let html = readFileSync(path.join(distPath, 'index.html'), 'utf8');
+  html = html.replace('</title>', '</title>\n    <script src="/service-worker.js"></script>')
+  writeFileSync(path.join(distPath, 'index.html'), html);
+  generateSW();
 }
 
 ;(async function() {
   try {
     console.log('Building UI...');
     await buildUI();
+    generateServiceWorker();
     console.log('Done!');
   } catch (err) {
     console.error(err);
---
+++ src/sw.template.js
@@ -0,0 +1,34 @@
+const cacheData = ["/sw.js"];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('siteCacheV3').then(cache => {
+      return cache.addAll(cacheData)
+    })
+  )
+})
+
+self.addEventListener('active', e => {
+  e.waitUntil(
+    caches.keys().then(cacheName => {
+      return Promise.all(
+        cacheName.filter(cacheName => cacheName !== 'site-cache')
+        .map(cacheName => {
+          return caches.delete(cacheName)
+        })
+      )
+    })
+  )
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+    .then(res => {
+      if (res) {
+        return res
+      }
+      return fetch(e.request)
+    })
+  )
+})
