d556517cd92e68c5ef6896b251cc898e2cfcdccb
navetacandra <naveta.cand@gmail.com>
2024-10-29 04:22:59 +0700

    refac: add server error response

--- src/api/controller/force-logout.js
+++ src/api/controller/force-logout.js
@@ -5,27 +5,36 @@ exports.forceLogout = async (req, res) => {
   const { token } = req.body;
   const ua = req.headers["user-agent"]; // Get user agent
 
-  const sessionToken = await conn.query(
-    `WITH actor AS (
-      SELECT * FROM sessions 
-      WHERE token=$1 AND ua=$2
-    ) 
-    SELECT * FROM sessions WHERE user_id=(SELECT user_id FROM actor) AND token=$3;`,
-    [actorToken, ua, token],
-  );
-  if (sessionToken.rowCount < 1) {
-    return res.status(400).json({ status: "error", message: "Invalid token" }); // Send 401 when token not found
-  }
+  try {
+    const sessionToken = await conn.query(
+      `WITH actor AS (
+        SELECT * FROM sessions 
+        WHERE token=$1 AND ua=$2
+      ) 
+      SELECT * FROM sessions WHERE user_id=(SELECT user_id FROM actor) AND token=$3;`,
+      [actorToken, ua, token],
+    );
+    if (sessionToken.rowCount < 1) {
+      return res
+        .status(400)
+        .json({ status: "error", message: "Invalid token" }); // Send 401 when token not found
+    }
 
-  await conn.query(`DELETE FROM sessions WHERE token=$1;`, [token]);
-  const response = res.status(200);
-  if (actorToken == token) {
-    response.clearCookie("token");
-    actorToken = null;
+    await conn.query(`DELETE FROM sessions WHERE token=$1;`, [token]);
+    const response = res.status(200);
+    if (actorToken == token) {
+      response.clearCookie("token");
+      actorToken = null;
+    }
+    response.json({
+      status: "success",
+      message: "Logged out",
+      data: { token: actorToken },
+    });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ status: "error", message: "Internal server error" });
   }
-  response.json({
-    status: "success",
-    message: "Logged out",
-    data: { token: actorToken },
-  });
 };
--- src/api/controller/logout.js
+++ src/api/controller/logout.js
@@ -4,23 +4,28 @@ exports.logout = async (req, res) => {
   const token = req.headers["x-access-token"] || req.cookies["token"]; // Get user token
   const ua = req.headers["user-agent"]; // Get user agent
 
-  const sessionToken = await conn.query(
-    `SELECT * FROM sessions WHERE token=$1 AND ua=$2;`,
-    [token, ua],
-  );
-  if (sessionToken.rowCount < 1) {
-    return res
-      .status(401)
-      .json({
+  try {
+    const sessionToken = await conn.query(
+      `SELECT * FROM sessions WHERE token=$1 AND ua=$2;`,
+      [token, ua],
+    );
+    if (sessionToken.rowCount < 1) {
+      return res.status(401).json({
         status: "error",
         message: "Unauthorized",
         code: "auth/unauthorized",
       }); // Send 401 when token not found
-  }
+    }
 
-  await conn.query(`DELETE FROM sessions WHERE token=$1;`, [token]);
-  return res
-    .status(200)
-    .clearCookie("token")
-    .json({ status: "success", message: "Logged out" });
+    await conn.query(`DELETE FROM sessions WHERE token=$1;`, [token]);
+    return res
+      .status(200)
+      .clearCookie("token")
+      .json({ status: "success", message: "Logged out" });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ status: "error", message: "Internal server error" });
+  }
 };
