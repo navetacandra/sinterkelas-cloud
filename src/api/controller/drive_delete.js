3dd7c0e5deca818e9bf8d2f4d7b5b0234a9517c2
navetacandra <naveta.cand@gmail.com>
2024-11-02 22:24:42 +0700

    fix: fixing deletion logic

--- src/api/controller/drive_delete.js
+++ src/api/controller/drive_delete.js
@@ -24,35 +24,72 @@ exports.drive_delete = async function (req, res) {
         .json({ status: "error", message: "Item not found" });
     }
 
-    const media = await conn.query(
+    const medias = await conn.query(
       `
-      SELECT cm.*, ci.id FROM cloud_items ci JOIN cloud_medias cm ON cm.item_id=ci.id WHERE ci.id=$1 AND ci.user_id = (SELECT user_id FROM sessions WHERE token = $2);
-    `,
-      [id, token],
+      WITH session AS (
+        SELECT user_id FROM sessions WHERE token = $1
+      ),
+      parent AS (
+        SELECT 
+          *, CONCAT(path, name, '/') AS full_path 
+        FROM cloud_items 
+        WHERE id = $2
+        AND user_id = (SELECT user_id FROM session)
+      ), 
+      children AS (
+        SELECT 
+          *, CONCAT(path, name) AS full_path 
+        FROM cloud_items 
+        WHERE path ~* CONCAT('^', (SELECT full_path FROM parent))
+        AND user_id = (SELECT user_id FROM session)
+      ),
+      items AS (
+        SELECT * FROM parent 
+          UNION ALL 
+        SELECT * FROM children
+      )
+      SELECT 
+        i.id AS item_id, 
+        cm.id AS media_id, 
+        cm.local_path, 
+        i.full_path 
+      FROM items i 
+      LEFT JOIN cloud_medias cm 
+        ON i.id = cm.item_id;
+        `,
+      [token, id],
     );
-    if (media.rowCount < 1) {
+    if (medias.rowCount < 1) {
       await conn.query("ROLLBACK");
       return res
         .status(404)
         .json({ status: "error", message: "Item not found" });
     }
 
-    await conn.query(
-      `
-      DELETE FROM cloud_items WHERE id=$1 AND user_id = (SELECT user_id FROM sessions WHERE token = $2);
-    `,
-      [id, token],
-    );
+    const item_paths = medias.rows
+      .map((media) => media.full_path)
+      .filter((f) => f);
+    const item_id = medias.rows.map((media) => media.item_id).filter((f) => f);
+    const item_id_q = item_id.map((_, i) => `$${i + 1}`).join(", ");
+    const media_id = medias.rows
+      .map((media) => media.media_id)
+      .filter((f) => f);
+    const media_id_q = media_id.map((_, i) => `$${i + 1}`).join(", ");
 
     await conn.query(
-      `
-      DELETE FROM cloud_medias WHERE item_id=$1;
-    `,
-      [id],
+      `DELETE FROM cloud_items WHERE id IN (${item_id_q});`,
+      item_id,
+    );
+    await conn.query(
+      `DELETE FROM cloud_medias WHERE id IN (${media_id_q});`,
+      media_id,
     );
 
-    if (existsSync(media.rows[0].local_path))
-      unlinkSync(media.rows[0].local_path);
+    for (const path of item_paths) {
+      if (existsSync(path)) {
+        unlinkSync(path);
+      }
+    }
 
     await conn.query("COMMIT");
     return res.status(200).json({ status: "success", message: "Item renamed" });
